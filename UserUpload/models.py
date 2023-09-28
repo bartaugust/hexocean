fee8bf659e2894401c678537e9278434958380b4
Bartosz Augustyniak <bartagst@gmail.com>
2023-09-28 16:05:55 +0200

    added user model

--- UserUpload/models.py
+++ UserUpload/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    BASIC = 1
+    PREMIUM = 2
+    ENTERPRISE = 3
+
+    TIER_CHOICES = (
+        (BASIC, 'Basic'),
+        (PREMIUM, 'Premium'),
+        (ENTERPRISE, 'Enterprise'),
+    )
+    tier = models.PositiveSmallIntegerField(choices=TIER_CHOICES, blank=True, null=True)
