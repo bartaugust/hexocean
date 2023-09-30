d07e14e0a7e3162efea3fc12dbdec9346f7d079c
Bartosz Augustyniak <bartagst@gmail.com>
2023-09-30 17:25:15 +0200

    added api view

--- UserUpload/views.py
+++ UserUpload/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
 
-# Create your views here.
+from .models import UploadedImage
+from .serializers import UploadedImageSerializer
+
+
+class ImageViewSet(ViewSet):
+    queryset = UploadedImage.objects.all()
+    serializer_class = UploadedImageSerializer
+
+# class ItemViewSet(ViewSet):
+#     queryset = Item.objects.all()
+#
+#     def list(self, request):
+#         serializer = ItemSerializer(self.queryset, many=True)
+#         return Response(serializer.data)
+#
+#     def retrieve(self, request, pk=None):
+#         item = get_object_or_404(self.queryset, pk=pk)
+#         serializer = ItemSerializer(item)
+#         return Response(serializer.data)
