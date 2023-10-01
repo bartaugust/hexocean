86182ad8ca3b364dee5e25f50644c3020095eccb
Bartosz Augustyniak <bartagst@gmail.com>
2023-10-01 12:55:22 +0200

    code cleanup

--- UserUpload/models.py
+++ UserUpload/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin, AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, AbstractUser
 from django.contrib.postgres.fields import ArrayField
 
 
--- UserUpload/views.py
+++ UserUpload/views.py
@@ -1,23 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework import status
-
 from .models import UploadedImage
 from .serializers import UploadedImageSerializer
 
 
 class ImageViewSet(ModelViewSet):
     serializer_class = UploadedImageSerializer
-
-    # queryset = UploadedImage.objects.all()
     permission_classes = [IsAuthenticated]
-    queryset = UploadedImage.objects.filter()
 
     def get_queryset(self):
         return UploadedImage.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
-        # thumbnails = self.request.user.tier.thumbnail_sizes
         serializer.save(user=self.request.user)
